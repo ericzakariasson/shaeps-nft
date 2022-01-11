54be0983f61f04865d954a9bad6f3816c8a4d014
ericzakariasson <eric.zakariasson@gmail.com>
2022-01-11 16:40:07 +0100

    update `_app`

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/spectral";
+import type { AppProps } from "next/app";
+import "../index.css";
+import { theme } from "../theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
