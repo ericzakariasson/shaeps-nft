6efda46fdec44ec34747c486c3c5ae7d740ec0b2
ericzakariasson <eric.zakariasson@gmail.com>
2022-01-11 18:03:54 +0100

    add web3 provider

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/spectral";
 import type { AppProps } from "next/app";
+import { Provider } from "wagmi";
 import "../index.css";
 import { theme } from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
     </ChakraProvider>
   );
 }
