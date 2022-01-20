ef61628e3838645a77d1975ba4ec26885e446deb
ericzakariasson <eric.zakariasson@gmail.com>
2022-01-20 22:39:40 +0100

    add some links

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -57,83 +57,106 @@ function Main() {
     // console.log("Minted", `https://rinkeby.etherscan.io/tx/${tx.hash}`);
   }
 
+  const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+
   return (
-    <Flex
-      as="main"
-      direction={["column", "column", "row"]}
-      alignItems={["unset", "unset", "flex-start"]}
-    >
-      <Box border="1px" flex="1" mr={["0", "0", "8"]} mb={["8", "8", "0"]}>
-        <Shaep colors={colors} />
-      </Box>
-      <Flex flex="1" direction="column">
-        <Box order={[1, 1, 0]} mb={["0", "0", "4"]}>
-          <Heading as="h2" size="md" mb="2">
-            what this is
-          </Heading>
-          <Text mb="2">
-            this project was created to bring some simple and elegant shapes to
-            the nft landscape. something easy, yet complex. something boring,
-            yet enough. take a look at{" "}
-            <Link
-              isExternal
-              href={`https://opensea.io/assets/matic/${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}`}
-              textDecoration="underline"
-              color="#2081e2"
-            >
-              OpenSea
-            </Link>{" "}
-            to see what has been minted so far
-          </Text>
-          <Text mb="2">
-            the art is generated on <Text as="i">the chain</Text> by selecting
-            colors based on your wallet address. there are a total of 531441
-            permutations (9 colors and 6 parts,{" "}
-            <Text as="i">
-              n<Text as="sup">r</Text>
-            </Text>
-            ), so the chance of your future shaep being unique is{" "}
-            <Text as="em">good</Text>
-          </Text>
-          <Text mb="2">some things that are good to know:</Text>
-          <UnorderedList
-            listStyleType="square"
-            listStylePosition="inside"
-            mb="4"
-          >
-            <ListItem>
-              there will be a total of {TOTAL_SUPPLY} Shaeps that can be minted.
-            </ListItem>
-            <ListItem>the cost of one Shaep will be 5 $MATIC</ListItem>
-          </UnorderedList>
+    <>
+      <Flex
+        as="main"
+        direction={["column", "column", "row"]}
+        alignItems={["unset", "unset", "flex-start"]}
+        mb="8"
+      >
+        <Box border="1px" flex="1" mr={["0", "0", "8"]} mb={["8", "8", "0"]}>
+          <Shaep colors={colors} />
         </Box>
-        <Spacer />
-        <Flex
-          order={[0, 0, 1]}
-          mb={["4", "4", "0"]}
-          direction={["row", "row", "column"]}
-          align={["end", "end", "unset"]}
-          justify={["space-between", "space-between", "unset"]}
-        >
-          <Text fontSize="lg" mb={[0, 0, "2"]}>
-            {AMOUNT_MINTED}/{TOTAL_SUPPLY} minted
-          </Text>
-          <Box>
-            <MintForm
-              totalSupply={TOTAL_SUPPLY}
-              mintedSupply={AMOUNT_MINTED}
-              onMint={handleMint}
-            />
+        <Flex flex="1" direction="column">
+          <Box order={[1, 1, 0]} mb={["0", "0", "4"]}>
+            <Heading as="h2" size="md" mb="2">
+              what this is
+            </Heading>
+            <Text mb="2">
+              this project was created to bring some simple and elegant shapes
+              to the nft landscape. something easy, yet complex. something
+              boring, yet enough. take a look at{" "}
+              <Link
+                isExternal
+                href={`https://opensea.io/assets/matic/${contractAddress}`}
+                textDecoration="underline"
+                color="#2081e2"
+              >
+                OpenSea
+              </Link>{" "}
+              to see what has been minted so far
+            </Text>
+            <Text mb="2">
+              the art is generated on <Text as="i">the chain</Text> by selecting
+              colors based on your wallet address. there are a total of 531441
+              permutations (9 colors and 6 parts,{" "}
+              <Text as="i">
+                n<Text as="sup">r</Text>
+              </Text>
+              ), so the chance of your future shaep being unique is{" "}
+              <Text as="em">good</Text>
+            </Text>
+            <Text mb="2">some things that are good to know:</Text>
+            <UnorderedList
+              listStyleType="square"
+              listStylePosition="inside"
+              mb="4"
+            >
+              <ListItem>
+                there will be a total of {TOTAL_SUPPLY} Shaeps that can be
+                minted.
+              </ListItem>
+              <ListItem>the cost of one Shaep will be 5 $MATIC</ListItem>
+            </UnorderedList>
           </Box>
+          <Flex
+            order={[0, 0, 1]}
+            mb={["4", "4", "0"]}
+            direction={["row", "row", "column"]}
+            align={["end", "end", "unset"]}
+            justify={["space-between", "space-between", "unset"]}
+          >
+            <Text fontSize="lg" mb={[0, 0, "2"]}>
+              {AMOUNT_MINTED}/{TOTAL_SUPPLY} minted
+            </Text>
+            <Box>
+              <MintForm
+                totalSupply={TOTAL_SUPPLY}
+                mintedSupply={AMOUNT_MINTED}
+                onMint={handleMint}
+              />
+            </Box>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+      <Flex as="footer">
+        <Link isExternal href="https://twitter.com/ericzakariasson">
+          twitter
+        </Link>
+        <Text mx="2">—</Text>
+        <Link isExternal href="https://github.com/ericzakariasson/shaeps-nft">
+          github
+        </Link>
+        <Spacer />
+        <Link
+          isExternal
+          href={`https://polygonscan.com/token/${contractAddress}`}
+          color="gray"
+          fontSize="sm"
+        >
+          {contractAddress}
+        </Link>
+      </Flex>
+    </>
   );
 }
 
 export default function Index() {
   return (
-    <Container maxW="container.lg" py={["4", "4", "16"]}>
+    <Container maxW="container.lg" py={["4", "4", "12"]}>
       <Header />
       <Main />
     </Container>
