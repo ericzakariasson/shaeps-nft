87aec33e4c28a61709556ed389c7583179f8bb8e
ericzakariasson <eric.zakariasson@gmail.com>
2022-01-20 23:39:01 +0100

    use provider for reading contract

--- apps/web/hooks/useShaepSupply/useShaepSupply.ts
+++ apps/web/hooks/useShaepSupply/useShaepSupply.ts
@@ -1,22 +1,23 @@
-import { useContractRead } from "wagmi";
-
+import { useContractRead, useProvider } from "wagmi";
 import { Shaeps__factory } from "../../contract/types/factories/Shaeps__factory";
 import { Shaeps } from "../../contract/types/Shaeps";
 
 export function useShaepSupply() {
+  const provider = useProvider();
+
+  const contractConfig = {
+    addressOrName: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    contractInterface: Shaeps__factory.abi,
+    signerOrProvider: provider,
+  };
+
   const [totalSupplyRequest] = useContractRead<Shaeps>(
-    {
-      addressOrName: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-      contractInterface: Shaeps__factory.abi,
-    },
+    contractConfig,
     "getTotalSupply"
   );
 
   const [mintedSupplyRequest] = useContractRead<Shaeps>(
-    {
-      addressOrName: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-      contractInterface: Shaeps__factory.abi,
-    },
+    contractConfig,
     "getMintedSupply"
   );
 
