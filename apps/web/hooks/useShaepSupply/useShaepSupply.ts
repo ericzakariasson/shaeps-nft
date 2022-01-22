ca326c43451d57535358a3185823eba183e10acc
ericzakariasson <eric.zakariasson@gmail.com>
2022-01-23 00:40:21 +0100

    add hack for failing contract call

--- apps/web/hooks/useShaepSupply/useShaepSupply.ts
+++ apps/web/hooks/useShaepSupply/useShaepSupply.ts
@@ -3,6 +3,15 @@ import { useContract, useProvider } from "wagmi";
 import { Shaeps__factory } from "../../contract/types/factories/Shaeps__factory";
 import { Shaeps } from "../../contract/types/Shaeps";
 
+async function tryCatch<T>(fn: () => Promise<T>) {
+  let result: T | null = null;
+  try {
+    result = await fn();
+  } catch (errro) {}
+
+  return result;
+}
+
 export function useShaepSupply() {
   const [mintedSupply, setMintedSupply] = useState<number | null>(null);
   const [maxSupply, setMaxSupply] = useState<number | null>(null);
@@ -22,13 +31,16 @@ export function useShaepSupply() {
 
   useEffect(() => {
     async function fetchInitialState() {
-      const maxSupplyResponse = await contract.maxSupply();
-      const mintedSupplyResponse = await contract.mintedSupply();
-      const priceResponse = await contract.price();
+      const [maxSupplyResponse, mintedSupplyResponse, priceResponse] =
+        await Promise.all([
+          tryCatch(contract.maxSupply),
+          tryCatch(contract.mintedSupply),
+          tryCatch(contract.price),
+        ]);
 
-      setMaxSupply(maxSupplyResponse.toNumber());
-      setMintedSupply(mintedSupplyResponse.toNumber());
-      setPrice(priceResponse.toNumber());
+      setMaxSupply(maxSupplyResponse?.toNumber());
+      setMintedSupply(mintedSupplyResponse?.toNumber());
+      setPrice(priceResponse?.toNumber());
     }
     fetchInitialState();
 
