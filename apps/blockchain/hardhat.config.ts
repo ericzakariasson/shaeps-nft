bbc419ff88c6aa50badb59ab6770123b2da33743
ericzakariasson <eric.zakariasson@gmail.com>
2022-01-24 21:25:16 +0100

    use ts files

---
+++ apps/blockchain/hardhat.config.ts
@@ -0,0 +1,34 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "dotenv/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+const accounts =
+  process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts,
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
