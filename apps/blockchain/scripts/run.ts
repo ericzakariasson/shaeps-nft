120a2d5eefdbc372c2b44eb731d7a97d7f3c0342
ericzakariasson <eric.zakariasson@gmail.com>
2022-02-01 22:19:30 +0100

    update run script

--- apps/blockchain/scripts/run.ts
+++ apps/blockchain/scripts/run.ts
@@ -1,25 +1,34 @@
 import { ethers } from "hardhat";
 
+const COUNT = 10;
+
 async function main() {
+  const [owner] = await ethers.getSigners();
+
+  const collector = owner.address;
+
+  const name = "Shaeps";
+  const symbol = "SHAEPS";
+  const paused = false;
+
   const Shaeps = await ethers.getContractFactory("Shaeps");
 
-  const shaeps = await Shaeps.deploy(
-    "Shaeps",
-    "SHAEPS",
-    "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-  );
+  const shaeps = await Shaeps.deploy(name, symbol, collector, paused);
 
   await shaeps.deployed();
 
   console.log("Shaeps deployed to:", shaeps.address);
 
-  for (let i = 0; i < 111; i++) {
+  for (let i = 0; i < COUNT; i++) {
     const tx = await shaeps.mint("0x70997970c51812dc3a010c7d01b50e0d17dc79c8", {
-      value: 1000000000000000,
+      value: ethers.utils.parseEther("1"),
     });
 
     await tx.wait();
     console.log("NFT minted", i);
+    const metadata = await shaeps.generateMetadata(i);
+    console.log(metadata);
+    console.log("\n");
   }
 }
 
